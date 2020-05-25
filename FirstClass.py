4bdb9e5a2302c3f1a279fe06de75629542e4a398
chisoftltd <chisoftltd@gmail.com>
2020-05-25 14:10:15 +0200

    # Integer and string Data Type # Float and string Data Type

--- FirstClass.py
+++ FirstClass.py
@@ -2,33 +2,53 @@
 # Written on 25th may 2020
 # First class in Day 2
 
-name = "ChisoftMedia"
+# name = "ChisoftMedia"
 
-print("My name is " + name)
-print("Thank you")
+# print("My name is " + name)
+# print("Thank you")
 
 # Indentation
-a = 10
-b = 0
-if (a == b):
-   print("a equals b")
-else:
-   print("a is not equal to b")
+# a = 10
+# b = 0
+# if (a == b):
+#    print("a equals b")
+# else:
+#    print("a is not equal to b")
 
 # Multi-line Statements in Python
-title = "Mr. "
-firstname = "Benjamin "
-lastname = "Chinwe "
-othername = "Uchenna "
-fullname = title + \
-           firstname + \
-           lastname + \
-           othername
-print(fullname)
+# title = "Mr. "
+# firstname = "Benjamin "
+# lastname = "Chinwe "
+# othername = "Uchenna "
+# fullname = title + \
+#            firstname + \
+#            lastname + \
+#            othername
+# print(fullname)
 
-myName = input("Please enter your name? ")
-if not myName:
-    myName = input("Please you need to enter your name? ")
+# myName = input("Please enter your name? ")
+# if not myName:
+#     myName = input("Please you need to enter your name? ")
 
-if myName:
-    print("Your name is: " + myName)
+# if myName:
+#    print("Your name is: " + myName)
+
+
+# Integer and string Data Type
+number1 = input("Please enter a first number :")
+number2 = input("Please enter a second number :")
+
+sum = int(number1) + int(number2)
+
+print("The sum of "+ number1 + " and " + number2 + " = " + str(sum))
+print(sum)
+
+
+# Float and string Data Type
+number1 = input("Please enter a first score :")
+number2 = input("Please enter a second score :")
+
+sum = float(number1) + float(number2)
+
+print("The sum of "+ number1 + " and " + number2 + " = " + str(sum))
+print(sum)
