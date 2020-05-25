42ea9135488f2725b114812597b794144683d087
chisoftltd <chisoftltd@gmail.com>
2020-05-25 13:46:06 +0200

    Python print statement, assignment statement, If ... else statement

---
+++ FirstClass.py
@@ -0,0 +1,27 @@
+# My name is Benjamin C.
+# Written on 25th may 2020
+# First class in Day 2
+
+name = "ChisoftMedia"
+
+print("My name is " + name)
+print("Thank you")
+
+# Indentation
+a = 10
+b = 0
+if (a == b):
+   print("a equals b")
+else:
+   print("a is not equal to b")
+
+# Multi-line Statements in Python
+title = "Mr. "
+firstname = "Benjamin "
+lastname = "Chinwe "
+othername = "Uchenna "
+fullname = title + \
+           firstname + \
+           lastname + \
+           othername
+print(fullname)
